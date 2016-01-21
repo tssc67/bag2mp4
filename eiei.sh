6327e8abdff8902a881ef73302c7646c4fdb9c2d
Teerapath Sujitto <teerapart.s@gmail.com>
2016-01-22 03:02:17 +0700

    Finished converter

--- eiei.sh
+++ eiei.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ bag2mp4.cpp -o bag2mp4 -L/opt/ros/indigo/lib   -lrosbag_storage -lrostime -lcpp_common -lroscpp -Wl,-rpath,/opt/ros/indigo/lib -lroscpp_serialization
+g++ bag2mp4.cpp -o bag2mp4 -L/opt/ros/indigo/lib   -lrosbag_storage -lrostime -lcpp_common -lroscpp -Wl,-rpath,/opt/ros/indigo/lib -lroscpp_serialization `pkg-config --libs opencv` -std=c++11
